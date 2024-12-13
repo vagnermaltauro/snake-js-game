37e06792851ec03bf8a9c04bb4b258aec381732b
vagnermaltauro <vagnermaltauro@gmail.com>
2024-12-13 15:59:30 -0300

    feat: implement changeDirection function

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ function gameStart() {
     gameScore.textContent = score;
     createFood();
     drawFood();
+    nextTick();
 }
 
 /**
@@ -133,7 +134,37 @@ function drawSnake() {
     })
 }
 
-function changeDirection() { }
+function changeDirection(event) {
+    const keyPressed = event.keyCode;
+    const LEFT = 37;
+    const UP = 38;
+    const RIGHT = 39;
+    const DOWN = 40;
+
+    const goingUp = yVelocity === -unitSize;
+    const goingDown = yVelocity === unitSize;
+    const goingRight = xVelocity === unitSize;
+    const goingLeft = xVelocity === -unitSize;
+
+    switch(true) {
+        case keyPressed === LEFT && !goingRight:
+            xVelocity = -unitSize;
+            yVelocity = 0;
+            break;
+        case keyPressed === UP && !goingDown:
+            xVelocity = 0;
+            yVelocity = -unitSize;
+            break;
+        case keyPressed === RIGHT && !goingLeft:
+            xVelocity = unitSize;
+            yVelocity = 0;
+            break;
+        case keyPressed === DOWN && !goingUp:
+            xVelocity = 0;
+            yVelocity = unitSize;
+            break;
+    }
+}
 
 function checkGameOver() { }
 
